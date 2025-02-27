f8a8d4971a0c5f02176a25d5bee805d3c91f389f
langkenan <2650007656@qq.com>
2025-02-27 11:29:52 +0800

    工作目录创建

--- EditEnd/app/__init__.py
+++ EditEnd/app/__init__.py
@@ -13,6 +13,20 @@ def create_app():
     from .routes import main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .blueprints.hzxRoutes import hzx_blueprint
+    app.register_blueprint(hzx_blueprint)
+
+    from .blueprints.lhRoutes import lh_blueprint
+    app.register_blueprint(lh_blueprint)
+
+    from .blueprints.lknRoutes import lkn_blueprint
+    app.register_blueprint(lkn_blueprint)
+
+    from .blueprints.lukeRoutes import luke_blueprint
+    app.register_blueprint(luke_blueprint)
+
+    from .blueprints.whlRoutes import whl_blueprint
+    app.register_blueprint(whl_blueprint)
 
     return app
 
--- EditFront/front/src/main.js
+++ EditFront/front/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import router from './router'; // 引入路由实例
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios  // 全局挂载
+//使用路由
+app.use(router);
 app.mount('#app')
+
